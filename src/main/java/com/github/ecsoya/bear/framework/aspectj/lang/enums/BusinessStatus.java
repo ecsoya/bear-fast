9a6017159aed2b82c5e1630be5d04c76f6d2c0e0
AngryRED <jin.liu@soyatec.com>
2023-08-22 09:31:15 +0800

    Init

---
+++ src/main/java/com/github/ecsoya/bear/framework/aspectj/lang/enums/BusinessStatus.java
@@ -0,0 +1,19 @@
+package com.github.ecsoya.bear.framework.aspectj.lang.enums;
+
+/**
+ * 操作状态
+ * 
+ * @author angryred
+ *
+ */
+public enum BusinessStatus {
+	/**
+	 * 成功
+	 */
+	SUCCESS,
+
+	/**
+	 * 失败
+	 */
+	FAIL,
+}
---
+++ src/main/java/com/github/ecsoya/bear/project/system/controller/SysIndexController.java
@@ -0,0 +1,29 @@
+package com.github.ecsoya.bear.project.system.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.ecsoya.bear.common.utils.StringUtils;
+import com.github.ecsoya.bear.framework.config.GlobalConfig;
+
+/**
+ * 首页
+ *
+ * @author angryred
+ */
+@RestController
+public class SysIndexController {
+	/** 系统基础配置 */
+	@Autowired
+	private GlobalConfig globalConfig;
+
+	/**
+	 * 访问首页，提示语
+	 */
+	@RequestMapping("/")
+	public String index() {
+		return StringUtils.format("欢迎使用{}后台管理框架，当前版本：v{}，请通过前端地址访问。", globalConfig.getName(),
+				globalConfig.getVersion());
+	}
+}

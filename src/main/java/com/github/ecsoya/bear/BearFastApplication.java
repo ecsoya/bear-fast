30ac04153c88915d4f9ac98430b56a26aca50f25
AngryRED <jin.liu@soyatec.com>
2023-08-23 08:32:36 +0800

    maria-db

--- src/main/java/com/github/ecsoya/bear/BearFastApplication.java
+++ src/main/java/com/github/ecsoya/bear/BearFastApplication.java
@@ -1,18 +1,18 @@
-package com.github.ecsoya.bear;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-/**
- * 启动程序
- * 
- * @author angryred
- */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class BearFastApplication {
-	public static void main(String[] args) {
-		// System.setProperty("spring.devtools.restart.enabled", "false");
-		SpringApplication.run(BearFastApplication.class, args);
-	}
-}
+//package com.github.ecsoya.bear;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//
+///**
+// * 启动程序
+// * 
+// * @author angryred
+// */
+//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+//public class BearFastApplication {
+//	public static void main(String[] args) {
+//		// System.setProperty("spring.devtools.restart.enabled", "false");
+//		SpringApplication.run(BearFastApplication.class, args);
+//	}
+//}

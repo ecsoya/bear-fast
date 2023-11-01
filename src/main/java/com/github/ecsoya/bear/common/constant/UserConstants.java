9b44aa90eaba0d12b26ebbdb29f558eba0655899
AngryRED <jin.liu@soyatec.com>
2023-11-01 11:10:04 +0800

    修复BUG

--- src/main/java/com/github/ecsoya/bear/common/constant/UserConstants.java
+++ src/main/java/com/github/ecsoya/bear/common/constant/UserConstants.java
@@ -67,7 +67,7 @@ public class UserConstants {
 	 * 用户名长度限制
 	 */
 	public static final int USERNAME_MIN_LENGTH = 2;
-	public static final int USERNAME_MAX_LENGTH = 20;
+	public static final int USERNAME_MAX_LENGTH = 50;
 
 	/**
 	 * 密码长度限制

d65f9877e58a24ddfa76b0fe4f12e26201599e49
AngryRED <jin.liu@soyatec.com>
2024-06-26 18:41:45 +0800

    File Upload

--- src/main/java/com/github/ecsoya/bear/common/utils/file/FileUploadUtils.java
+++ src/main/java/com/github/ecsoya/bear/common/utils/file/FileUploadUtils.java
@@ -110,9 +110,7 @@ public class FileUploadUtils {
 	 * 编码文件名
 	 */
 	public static final String extractFilename(MultipartFile file) {
-		return StringUtils.format("{}/{}_{}.{}", DateUtils.datePath(),
-				FilenameUtils.getBaseName(file.getOriginalFilename()), Seq.getId(Seq.uploadSeqType),
-				getExtension(file));
+		return StringUtils.format("{}/{}.{}", DateUtils.datePath(), Seq.getId(Seq.uploadSeqType), getExtension(file));
 	}
 
 	public static final File getAbsoluteFile(String uploadDir, String fileName) throws IOException {
